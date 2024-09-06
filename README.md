0dc4d89ae8a994cdc1e136d738ae71c0051739b6
fwenhua <fwenhua@qualicom.com.cn>
2024-09-06 13:08:28 +0800

    + init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### 使用说明
+使用cocos打包web-desktop,然后放置到[cocosExport/web-desktop/]该目录下
+
+在index.js中可以修改分辨率和标题
+
+const baseConfig = {
+    width: 768,
+    height: 768,
+    windowTile:'DemoTitle'
+}
---
+++ index.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron')
+
+const baseConfig = {
+  width: 768,
+  height: 768,
+  windowTile:'DemoTitle'
+}
+
+const windowConfig = {
+  width: baseConfig.width,
+  height: baseConfig.height,
+  show: true,
+  fullscreen: false,
+  resizable: false,
+  frame: true,
+  title: baseConfig.title
+}
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+app.on('ready', function() {
+
+    mainWindow = new BrowserWindow(windowConfig);
+
+    mainWindow.loadURL('file://' + __dirname + '/cocosExport/web-desktop/index.html').then(() =>{
+       console.info('load index.html success')
+    });
+
+    mainWindow.removeMenu();
+
+    mainWindow.on('closed', function() {
+      mainWindow = null;
+    });
+});
